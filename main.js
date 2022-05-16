580ac2457f0769262c4c2913d32122006040cbc6
Pontes Neto Dev <pontesneto2@gmail.com>
2022-05-15 21:02:09 -0300

    add sheet of main.js

---
+++ main.js
@@ -0,0 +1,45 @@
+<script>
+
+    console.log("Boletim dos Alunos")
+    console.log("Aluno 1")
+let n1, n2;
+    console.log("Nota da primeira prova foi 7")
+n1 = 7;
+    console.log("Nota da segunda prova foi 8")
+n2 = 8;
+    console.log("A média do colégio é 7... então:")
+media = (n1 + n2) / 2;
+
+    if(media >= 7){
+        console.log("Aluno Aprovado!!!")
+    }else{
+        console.log("Aluno Reprovado!!!")
+    }
+    
+    console.log("Aluno 2")
+    console.log("Nota da primeira prova foi 5")
+n1 = 5;
+    console.log("Nota da segunda prova foi 4")
+n2 = 4;
+    console.log("A média do colégio é 7... então:")
+media = (n1 + n2) / 2;
+    if(media >= 7){
+        console.log("Aluno Aprovado!!!")
+    }else{
+        console.log("Aluno Reprovado!!!")
+    }
+
+    console.log("Aluno 3")
+    console.log("Nota da primeira prova foi 10")
+n1 = 10;
+    console.log("Nota da segunda prova foi 6")
+n2 = 6;
+    console.log("A média do colégio é 7... então:")
+media = (n1 + n2) / 2;
+    if(media >= 7){
+        console.log("Aluno Aprovado!!!")
+    }else{
+        console.log("Aluno Reprovado!!!")
+    }
+
+</script>
